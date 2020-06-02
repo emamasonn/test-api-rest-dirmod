09f6bae97bc6d1f55076c8b1f2f8cc93b4162bb1
David Mason <emamasonn@gmail.com>
2020-06-02 17:48:15 -0300

    add config

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config')
+
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -7,12 +9,7 @@ const Axios = require('axios')
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
-app.use(bodyParser.json())
-
-
-process.env.URL_KEY = process.env.URL_KEY || '4469|pMQOWBby^qYs0rbPkOn53aa^9SnSb6UM'
-process.env.PORT = process.env.PORT || 3000
-process.env.URL_BASE = process.env.URL_BASE || 'https://api.cambio.today/v1/quotes' 
+app.use(bodyParser.json()) 
 
 app.get('/cotizacion/dolar', (req, res) => {
     Axios.get(`${process.env.URL_BASE}/USD/ARS/json?quantity=1&key=${process.env.URL_KEY}`)
