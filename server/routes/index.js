82ab869183217c6693ab4a3e46c8f16fe818d582
David Mason <emamasonn@gmail.com>
2020-06-02 18:11:47 -0300

    add routes

---
+++ server/routes/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/cotizacion/dolar', (req, res) => {
+    Axios.get(`${process.env.URL_BASE}/USD/ARS/json?quantity=1&key=${process.env.URL_KEY}`)
+        .then(response => res.json({
+            moneda: 'dolar',
+            precio: response.data.result.value
+        }))
+        .catch(err => res.status(400).json({
+            ok: false,
+            err
+        }))
+})
+
+
+app.get('/cotizacion/euro', (req, res) => {
+    Axios.get(`${process.env.URL_BASE}/EUR/ARS/json?quantity=1&key=${process.env.URL_KEY}`)
+        .then(response => res.json({
+            moneda: 'euro',
+            precio: response.data.result.value
+        }))
+        .catch(err => res.status(400).json({
+            ok: false,
+            err
+        }))
+})
+
+app.get('/cotizacion/real', (req, res) => {
+    Axios.get(`${process.env.URL_BASE}/BRL/ARS/json?quantity=1&key=${process.env.URL_KEY}`)
+        .then(response => res.json({
+            moneda: 'real',
+            precio: response.data.result.value
+        }))
+        .catch(err => res.status(400).json({
+            ok: false,
+            err
+        }))
+})
+
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -10,43 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json()) 
-
-app.get('/cotizacion/dolar', (req, res) => {
-    Axios.get(`${process.env.URL_BASE}/USD/ARS/json?quantity=1&key=${process.env.URL_KEY}`)
-        .then(response => res.json({
-            moneda: 'dolar',
-            precio: response.data.result.value
-        }))
-        .catch(err => res.status(400).json({
-            ok: false,
-            err
-        }))
-})
-
-
-app.get('/cotizacion/euro', (req, res) => {
-    Axios.get(`${process.env.URL_BASE}/EUR/ARS/json?quantity=1&key=${process.env.URL_KEY}`)
-        .then(response => res.json({
-            moneda: 'euro',
-            precio: response.data.result.value
-        }))
-        .catch(err => res.status(400).json({
-            ok: false,
-            err
-        }))
-})
-
-app.get('/cotizacion/real', (req, res) => {
-    Axios.get(`${process.env.URL_BASE}/BRL/ARS/json?quantity=1&key=${process.env.URL_KEY}`)
-        .then(response => res.json({
-            moneda: 'real',
-            precio: response.data.result.value
-        }))
-        .catch(err => res.status(400).json({
-            ok: false,
-            err
-        }))
-})
+app.use(require("./routes/index"))
 
 app.listen(process.env.PORT, () => {
     console.log("Escuchando el puerto: ", process.env.PORT)
