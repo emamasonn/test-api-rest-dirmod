1520291b98f48cc6f18485c8c344fa185e0d2753
David Mason <emamasonn@gmail.com>
2020-06-03 15:44:53 -0300

    add cors

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,3 @@
 process.env.URL_KEY = process.env.URL_KEY || '4469|pMQOWBby^qYs0rbPkOn53aa^9SnSb6UM'
-process.env.PORT = process.env.PORT || 3000
+process.env.PORT = process.env.PORT || 8080
 process.env.URL_BASE = process.env.URL_BASE || 'https://api.cambio.today/v1/quotes'
--- server/server.js
+++ server/server.js
@@ -3,11 +3,10 @@ require('./config/config')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const Axios = require('axios')
-
+const cors = require('cors');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
- 
+app.use(cors({origin: '*'}));
 // parse application/json
 app.use(bodyParser.json()) 
 app.use(require("./routes/index"))
